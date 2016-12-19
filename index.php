dacfc859b97176520d2ce3d270ec577c742de260
nottypimp <pitiwat.meka@gmail.com>
2016-12-19 13:16:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,18 +15,22 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			
-			
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
+			if($text == 'holiday'){
+			$messages = [
+				'type' => 'text',
+				'text' => 'จันทร์-อังคาร   2-3  มกราคม   2560  วันปีใหม่'
+			}else{
 			$messages = [
 				'type' => 'text',
 				'text' => $text
 			];
-
+			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
